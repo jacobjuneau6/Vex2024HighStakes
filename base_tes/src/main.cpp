d5af6b38e840c325add05c5b0317e320fd047faa
Jacob Juneau <jacobjuneau6@gmail.com>
2024-07-24 10:30:11 -0700

    24/07/24

--- base_tes/src/main.cpp
+++ base_tes/src/main.cpp
@@ -35,13 +35,14 @@ motor back_left = motor(PORT11, ratio6_1, true);
 
 motor front_right = motor(PORT3, ratio6_1, false);
 
-motor mid_right = motor(PORT9, ratio6_1, false);
+motor mid_right = motor(PORT5, ratio6_1, false);
 
 motor back_right = motor(PORT20, ratio6_1, false);
 
 controller Controller1 = controller(primary);
 
-
+motor intake = motor(PORT7, ratio18_1, true);
+motor pull = motor(PORT8, ratio6_1, false);
 
 // Helper to make playing sounds from the V5 in VEXcode easier and
 // keeps the code cleaner by making it clear what is happening.
@@ -60,15 +61,27 @@ bool RemoteControlCodeEnabled = true;
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
-/*    Author:       {author}                                                  */
-/*    Created:      {date}                                                    */
+/*    Author:       Jacob Juneau                                              */
+/*    Created:      07/24/24                                                  */
 /*    Description:  V5 project                                                */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
 // Include the V5 Library
 #include "vex.h"
-  
+ void intakeon(){
+  intake.setVelocity(200, percent);
+  intake.spin(forward);
+ } 
+void intakeoff(){
+  intake.setVelocity(0, percent);
+}
+void up(){
+  pull.spin(forward);
+}
+void down(){
+  pull.stop();
+}
 // Allows for easier use of the VEX Library
 using namespace vex;
 void pneumatics_down(){
@@ -80,23 +93,21 @@ void pneumatics_up(){
   Brain.Screen.print("up");
 }
 int main() {
-  //front_left.spin(forward);
-  //mid_left.spin(forward);
-  //back_left.spin(forward);
-  //front_right.spin(forward);
-  //mid_right.spin(forward);
- //back_right.spin(forward);
- const int wheelTravel = 320;
+const int wheelTravel = 320;
 const int trackWidth = 320;
 const int wheelBase = 130;
-//motor_group driveL(front_left, mid_left, back_left);
-//motor_group driveR(front_right, mid_left, back_right);
 motor_group driveL(front_left, mid_left, back_left);
 motor_group driveR(front_right, mid_right, back_right);
 drivetrain myDrivetrain(driveL, driveR, wheelTravel, trackWidth, wheelBase, mm);
 while (true) {
   Controller1.ButtonL1.pressed(pneumatics_down);
   Controller1.ButtonR1.pressed(pneumatics_up);
+  Controller1.ButtonA.pressed(intakeon);
+  Controller1.ButtonB.pressed(intakeoff);
+  if (Controller1.ButtonX.pressing()){
+    pull.setVelocity(200, rpm);
+    pull.spin(forward);
+  } else {pull.stop();}
 
   driveL.setVelocity(Controller1.Axis3.position(),percent);
 
