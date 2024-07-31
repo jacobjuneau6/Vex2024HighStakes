af2b5cedf441eac07a97a62478862f22391dd391
Jacob Juneau <jacobjuneau6@gmail.com>
2024-07-31 13:09:00 -0700

    added autonomus project code

---
+++ auton/src/main.cpp
@@ -0,0 +1,116 @@
+//#pragma region VEXcode Generated Robot Configuration
+// Make sure all required headers are included.
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
+//#include "drive.h"
+
+#include "vex.h"
+
+using namespace vex;
+
+brain Brain;
+
+
+
+#define waitUntil(condition)                                                   \
+  do {                                                                         \
+    wait(5, msec);                                                             \
+  } while (!(condition))
+
+#define repeat(iterations)                                                     \
+  for (int iterator = 0; iterator < iterations; iterator++)
+
+
+digital_out pneumatic = digital_out(Brain.ThreeWirePort.H);
+
+
+motor front_left = motor(PORT10, ratio6_1, true);
+
+motor mid_left = motor(PORT2, ratio6_1, true);
+
+motor back_left = motor(PORT11, ratio6_1, true);
+
+motor front_right = motor(PORT3, ratio6_1, false);
+
+motor mid_right = motor(PORT5, ratio6_1, false);
+
+motor back_right = motor(PORT20, ratio6_1, false);
+
+controller Controller1 = controller(primary);
+
+motor intake = motor(PORT17, ratio18_1, true);
+
+motor pull = motor(PORT7, ratio6_1, false);
+
+
+void playVexcodeSound(const char *soundName) {
+  printf("VEXPlaySound:%s\n", soundName);
+  wait(5, msec);
+}
+
+
+
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+
+//#pragma endregion VEXcode Generated Robot Configuration
+
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       Jacob Juneau                                              */
+/*    Created:      07/24/24                                                  */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// Include the V5 Library
+
+ void intakeon(){
+  Brain.Screen.print('on');
+  intake.setVelocity(200, percent);
+  intake.spin(forward);
+ } 
+
+
+void intakeoff(){
+  intake.setVelocity(0, percent);
+}
+
+
+void up(){
+  pull.spin(forward);
+}
+
+
+void down(){
+  pull.stop();
+}
+// Allows for easier use of the VEX Library
+using namespace vex;
+
+void pneumatics_down(){
+  pneumatic.set(true);
+ // Brain.Screen.print("down");
+}
+
+int main(){
+motor_group driveL(front_left, mid_left, back_left);
+motor_group driveR(front_right, mid_right, back_right);
+const int wheelTravel = 300;
+const int trackWidth = 320;
+const int wheelBase = 20;
+
+
+
+
+
+
+
+
+
+
+}
